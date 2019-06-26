86954b2b342ca19c635e69159f3cba14a9b8b754
Minqing <chen7410@uw.edu>
2019-06-25 21:16:40 -0700

    refactor

--- src/model/CountSpace.java
+++ src/model/CountSpace.java
@@ -1,24 +1,28 @@
 package model;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
-import org.apache.hadoop.io.Text;
 
+@SuppressWarnings("deprecation")
 public class CountSpace extends UDF {
+	/**
+	 * count the number of ' ' in the given airportName.
+	 * @param airportName
+	 * @return for example "this1is2a3airport4name"
+	 */
 	public String evaluate(String airportName) {
 		if (airportName == null) {
 			return null;
 		}
 		int count = 0;
-		String result = "";
+		StringBuilder result = new StringBuilder();
 		for (int i = 0; i < airportName.length(); i++) {
 			if (airportName.charAt(i) == ' ') {
 				count++;
-				result += count;
+				result.append(count);
 			} else {
-				result += airportName.charAt(i);
+				result.append(airportName.charAt(i));
 			}
 		}
-		return result;
+		return result.toString();
 	}
 }
